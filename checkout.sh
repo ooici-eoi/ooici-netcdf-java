742eecaf1b724f74a06182b25f54509fe7e402cf
Christopher Mueller <cmueller@asascience.com>
2010-09-30 10:44:06 -0400

    Updated checkout.sh to retrieve the latest stable tag (currently REL-CDM-4.2.4). Added .gitignore - ignores the "docs" directory which contains unidata presentations and documents about the CDM and NetCDF Java

--- checkout.sh
+++ checkout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-svn co http://svn.unidata.ucar.edu/repos/thredds/trunk | tee checkout.log
+svn co http://svn.unidata.ucar.edu/repos/thredds/tag/REL-CDM-4.2.4 latest_stable | tee checkout.log
